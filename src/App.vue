1858b1af7304b82d561e6f6066d3f21160170d26
Daniel MBAPPE <mbappedaniel00@yahoo.fr>
2022-07-06 10:19:46 +0200

    Change App

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,18 @@
         />
       </video>
     </div>
+    <ais-search-box
+      :class-names="{
+        'ais-search-box': 'ais-search-box',
+      }"
+      placeholder="Search something here..."
+    />
     <div class="left-panel">
-      <h2>Brands</h2>
+      <!-- <h1>Brands ||</h1> -->
       <ais-refinement-list attribute="brand" show-more />
       <ais-configure :hitsPerPage="8" />
     </div>
     <div class="right-panel">
-      <ais-search-box
-        :class-names="{
-          'ais-search-box': 'ais-search-box',
-        }"
-        placeholder="Search something here..."
-      />
       <ais-hits
         :class-names="{
           'ais-Hits': 'MyHits',
@@ -28,22 +28,23 @@
         }"
       >
         <template v-slot:item="{ item }">
-          <div class="card-wrap">
-            <div class="card" :style="cardStyle">
-              <div class="card-bg" style="[cardBGTransform, cardBgImage]">
-                <img
-                  :src="
-                    item.image['thumbnail']
-                      ? item.image['thumbnail']
-                      : 'https://freepngimg.com/thumb/categories/627.png'
-                  "
-                  align="left"
-                  :alt="item.name"
-                />
-              </div>
-              <div class="card-info">
+          <div class="wrapper">
+            <div class="card">
+              <div class="front">
                 <h2>{{ item.name }}</h2>
               </div>
+              <div class="right"></div>
+            </div>
+            <div class="img-wrapper">
+              <img
+                :src="
+                  item.image['thumbnail']
+                    ? item.image['thumbnail']
+                    : 'https://freepngimg.com/thumb/categories/627.png'
+                "
+                align="left"
+                :alt="item.name"
+              />
             </div>
           </div>
         </template>
@@ -101,10 +102,8 @@ body {
 
 .left-panel {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
-  margin-top: 4em;
-  margin-left: -1em;
 }
 
 .left-panel h2 {
@@ -128,172 +127,138 @@ body {
 }
 
 .right-panel {
-  margin-top: 4em;
-  margin-right: 4em;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 }
 
+.wrapper {
+  width: 280px;
+  height: 480px;
+  perspective: 800px;
+  position: relative;
+}
+
+.card {
+  width: 240px;
+  height: 320px;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: translateZ(-140px);
+  transition: transform 350ms cubic-bezier(0.39, 0.575, 0.565, 1);
+  cursor: pointer;
+}
+
+.card > div {
+  position: absolute;
+  width: 240px;
+  height: 320px;
+  padding: 34px 21px;
+  transition: all 350ms cubic-bezier(0.39, 0.575, 0.565, 1);
+}
+
+.front {
+  transform: rotateY(0deg) translateZ(160px);
+  border-radius: 24px 3px 0 0;
+  box-shadow: rgba(255, 255, 255, 0.66) 0 10px 20px 0,
+    inset rgb(255, 255, 255) 0 0 0 2px, inset rgba(255, 255, 255, 0.5) 0 0 0 2px;
+  transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+}
+
+.right {
+  opacity: 0.08;
+  transform: rotateY(90deg) translateX(20px) translateZ(160px);
+  border-radius: 0 0 3px 24px;
+  box-shadow: rgba(255, 255, 255, 0.66) 0 10px 20px 0,
+    inset rgb(255, 255, 255) 0 0 0 2px, inset rgba(255, 255, 255, 0.5) 0 0 0 2px;
+  transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+}
+
+.card:hover {
+  transform: translateZ(-160px) rotateY(-90deg);
+}
+
+.card:hover .front {
+  opacity: 0;
+}
+
+.card:hover .right {
+  opacity: 1;
+}
+
+h2 {
+  font-size: 21px;
+  color: white;
+  transform: translateX(20px) translateY(200px);
+}
+
+img {
+  width: 15em;
+  height: 15em;
+  transform-origin: top;
+  transition: transform 300ms cubic-bezier(0.39, 0.575, 0.565, 1);
+  transition-delay: 100ms;
+  transform: rotateZ(13deg) skewX(3deg);
+  pointer-events: none;
+}
+
+.img-wrapper {
+  animation: float 4s cubic-bezier(0.39, 0.575, 0.565, 1) infinite alternate;
+  position: absolute;
+  top: 0;
+  right: 0;
+  pointer-events: none;
+  backface-visibility: hidden;
+}
+
+@keyframes float {
+  0% {
+    transform: translateZ(20px);
+  }
+  100% {
+    transform: translateY(-21px) translateX(-13px) translateZ(30px);
+  }
+}
+
+.card:hover ~ .img-wrapper img {
+  transform: scale(1.1) translateX(47%) translateY(70%) rotateZ(80deg);
+}
+
 .MyHitsList {
   display: grid;
-  gap: 2em;
+  gap: 5em;
   grid-template-rows: repeat(2, 22em);
   grid-template-columns: repeat(4, 15em);
   margin-top: 0;
-  margin-bottom: 1em;
+  margin-right: 5em;
+  margin-bottom: 2.5em;
 }
 
 .MyHitsItem {
   position: relative;
   display: flex;
   flex-direction: column;
-  margin-top: 2em;
   border-radius: 0.5em;
   transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
   transform: perspective(800px);
   transform-style: preserve-3d;
 }
 
-.card-wrap {
-  margin: 10px;
-  transition: 0.2s cubic-bezier(0.445, 0.05, 0.55, 0.95);
-  cursor: pointer;
-}
-
-.card-wrap:hover {
-  transform: scale(1.1);
-}
-
-.card-wrap:hover .card-info {
-  transform: translateY(0);
-}
-
-.card-wrap:hover .card-info p {
-  opacity: 1;
-}
-
-.card-wrap:hover .card-info,
-.card-wrap:hover .card-info p {
-  transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1);
-}
-
-.card-wrap:hover .card-info:after {
-  transition: 5s cubic-bezier(0.23, 1, 0.32, 1);
-  opacity: 1;
-  transform: translateY(0);
-}
-
-.card-wrap:hover .card-bg {
-  transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1),
-    opacity 5s cubic-bezier(0.23, 1, 0.32, 1);
-  opacity: 1;
-}
-
-.card-wrap:hover .card {
-  transition: 0.6s cubic-bezier(0.23, 1, 0.32, 1),
-    box-shadow 2s cubic-bezier(0.23, 1, 0.32, 1);
-  box-shadow: rgba(255, 255, 255, 0.2) 0 0 10px 2px, white 0 0 0 1px,
-    rgb(255, 255, 255) 0 10px 30px 0, inset rgb(255, 255, 255) 0 0 0 2px,
-    inset white 0 0 0 2px;
-}
-
-.card {
-  position: relative;
-  flex: 0 0 240px;
-  width: 240px;
-  height: 320px;
-  /* background-color: #333; */
-  overflow: hidden;
-  border-radius: 10px;
-  box-shadow: rgba(255, 255, 255, 0.66) 0 10px 20px 0,
-    inset rgb(255, 255, 255) 0 0 0 2px, inset rgba(255, 255, 255, 0.5) 0 0 0 2px;
-  transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
-}
-
-.card-bg {
-  opacity: 1;
-  position: absolute;
-  top: -20px;
-  left: -20px;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  transition: 1s cubic-bezier(0.445, 0.05, 0.55, 0.95),
-    opacity 5s 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
-  pointer-events: none;
-}
-
-.card-bg img {
-  margin-top: 2em;
-  width: 15em;
-  height: 15em;
-}
-
-.card-info h2 {
-  margin-top: 200px;
-}
-
-.card-info {
-  padding: 20px;
-  position: absolute;
-  bottom: 0;
-  color: #fff;
-  transform: translateY(40%);
-  transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-}
-.card-info p {
-  opacity: 0;
-  text-shadow: black 0 2px 3px;
-  transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-}
-.card-info * {
-  position: relative;
-  z-index: 1;
-}
-.card-info:after {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 0;
-  width: 100%;
-  height: 100%;
-  background-image: linear-gradient(
-    to bottom,
-    transparent 0%,
-    rgba(0, 0, 0, 0.6) 100%
-  );
-  background-blend-mode: overlay;
-  opacity: 0;
-  transform: translateY(100%);
-  transition: 5s 1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
-}
-
-.card-info h1 {
-  font-family: "Playfair Display";
-  font-size: 36px;
-  font-weight: 700;
-  text-shadow: rgba(0, 0, 0, 0.5) 0 10px 10px;
-}
-
 .ais-RefinementList-list {
+  display: flex;
+  flex-direction: row;
   list-style-type: none;
 }
 
 .ais-RefinementList-item {
-  display: block;
-  margin: 0.5em;
+  margin: 2em;
 }
 
 .ais-RefinementList-label {
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   transition: 0.25s all ease-in-out;
 }
@@ -306,6 +271,7 @@ body {
 }
 
 .ais-RefinementList-count {
+  display: none;
   background-color: rgb(227, 224, 224);
   border-radius: 5em;
   padding: 0.5em;
@@ -331,6 +297,7 @@ body {
 }
 
 .ais-SearchBox-input {
+  margin-top: 4em;
   text-align: center;
   width: 50em;
   height: 3em;
@@ -338,9 +305,10 @@ body {
 }
 
 .ais-InstantSearch {
-  display: grid;
-  grid-template-columns: 1fr 4fr;
-  grid-gap: 1em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 .hit-name {
